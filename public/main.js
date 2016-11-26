1f1df20896b83c47852bea829bdceccf0b15e205
Christopher Kramer <Hendrix1957@gmail.com>
2016-11-26 18:28:24 -0500

    "Hello, App App!"

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  const xPositions = [1, 50, 100, 150, 200, 250, 300, 350, 399]
+  const yPositions = [0, 100]
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
